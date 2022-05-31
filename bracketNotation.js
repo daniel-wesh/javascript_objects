06549bde00c8d38a890753354c8d173403181052
Daniel W <daniel.waweru@outlook.com>
2022-05-31 20:03:13 +0300

    Updated bracketNotation.js
    Using bracket notation to access a property which is stored as the value of a variable.

--- bracketNotation.js
+++ bracketNotation.js
@@ -10,3 +10,19 @@ const myObject2 = {
 console.log(myObject2["color name"])//Red
 console.log(myObject2["color code"])//#2e2e345
 console.log(myObject2["myColor"])//Green
+
+//Bracket notation can also be used to access a property which is stored as the value of a variable.
+//useful for iterating through an object's propertiesor when accessing a lookup table.
+const myObject3= {
+    "color name": "Red",
+    "color code": "#2e2e345",
+    "color meaning": "Danger",
+    "myColor":"Green"
+}
+
+//Assigning a property to the value of a new variable.
+const newVar = "color name"
+
+const myColor = myObject3[newVar]
+
+console.log(myColor)//Red
