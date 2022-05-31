c0ce28fb9cf9192fe257b5d8d553a0ea2018c3e0
Daniel W <daniel.waweru@outlook.com>
2022-06-01 00:38:42 +0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,4 +1,14 @@
-const myArray = [2,5,10,24,28,34,45,100,900,10000,2,3,4,6,78]
+//const myArray = [2,5,10,24,28,34,45,100,900,10000,2,3,4,6,78]
 
-console.log(myArray.length)
-console.log(myArray[14])
+//console.log(myArray.length)
+//console.log(myArray[14])
+
+const myArr = [6, 3, 12, 5, 8];
+
+// Only change code below this line
+let total=0;
+
+for(let i = 0;i< myArr.length;i++){
+  total = total + myArr[i]
+}
+console.log(total)
