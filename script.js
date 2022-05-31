a61d74a67ba0378f8b2bc0ed9f020b78970a5439
Daniel W <daniel.waweru@outlook.com>
2022-05-31 19:23:49 +0300

    adding html and js code
    Added a html boilerplate code, used script tag to connect it to the js file and wrote a simple object
    
    Used the first method of accessing JS properties(Use of dot notation)

---
+++ script.js
@@ -0,0 +1,11 @@
+//Accessing object properties with dot notation
+const sampleObj = {
+    propOne: "Dodoma",
+    propTwo:"Nairobi",
+    propThree:6,
+    propFour:["Kenya","Uganda","Somalia"]
+}
+const firstValue=sampleObj.propOne
+const secondValue=sampleObj.propTwo
+
+console.log(firstValue)
