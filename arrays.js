299db071d9d332848692dc92c1fa2da28566c09d
Daniel W <daniel.waweru@outlook.com>
2022-06-01 00:20:23 +0300

    added arrys.js
    Arrays Practices in JS

---
+++ arrays.js
@@ -0,0 +1,4 @@
+const myArray = [2,5,10,24,28,34,45,100,900,10000,2,3,4,6,78]
+
+console.log(myArray.length)
+console.log(myArray[14])
--- bracketNotation.js
+++ bracketNotation.js
@@ -1,12 +1,17 @@
 //Accessing object properties by using bracket notation
 //commonly used when the property of the object has a space in its name
 // We can also use bracket notation on object properties without spaces
+let color = "color_name"
+
 const myObject2 = {
-    "color name": "Red",
+    "color_name": "Red",
     "color code": "#2e2e345",
     "color meaning": "Danger",
     "myColor":"Green"
 }
+console.log(myObject2[color])
+
+
 //console.log(myObject2["color name"])//Red
 //console.log(myObject2["color code"])//#2e2e345
 //console.log(myObject2["myColor"])//Green
@@ -40,5 +45,5 @@ myObject4.fName = "James"
 myObject4.favFood = ["Seafood","Vegetable Salad","Black Beans"]
 //using the bracket Notation
 myObject4["Second Name"] = "Mwanzia"
-console.log(myObject4.favFood)
+//console.log(myObject4.favFood)
 
